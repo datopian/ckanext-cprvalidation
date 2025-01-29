076cf544bd1b56a8fce58d0a0e57fa97cc20af28
AlmubdyMutaikhan <mutaikhan.almubdy@gmail.com>
2025-01-29 23:40:16 +0500

    [fix][sm]: remove outdated and unused controller

--- ckanext/cprvalidation/controllers/csv.py
+++
@@ -1,9 +0,0 @@
-import os
-from ckan.logic import get_action
-from ckan.controllers.admin import AdminController
-
-class CprExportController(AdminController):
-
-    def download(self):
-        """Uses package_search action to get all datasets in JSON format and transform to CSV"""
-        pass
